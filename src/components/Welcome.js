36be21693d17d22a9af54ad4a2bfdf8a7a218971
abhilasha <abhilasha15597@gmail.com>
2022-11-05 23:12:51 +0530

    new

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ function Welcome(props){
   const name = props.name;
   return(
     <div>
-    <h1> hey ! {name} </h1>
+    <h1> hey {name} !</h1>
      <h2>Welcome to Newton School.</h2>
 </div>
 );
